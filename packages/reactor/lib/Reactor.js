2fc1a056e15eb21b8fd829bf328fc30122be5336
Vasily Kazantsev <vasily@kazantsev.xyz>
2020-06-16 16:56:13 +0300

    remove reactor js

--- packages/reactor/lib/Reactor.js
+++
@@ -1,2 +0,0 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.default=Reactor;function Reactor(){}
-//# sourceMappingURL=Reactor.js.map
--- packages/reactor/src/index.js
+++
@@ -1,3 +0,0 @@
-export { default } from './Reactor'
-export { default as Reagent } from './Reagent'
-export { default as Reaction } from './Reaction'
